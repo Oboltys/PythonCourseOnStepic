945b2af4970dc7aba97bde5d0721dc44e8b82fe5
Pavel <Oboltyss@Gmail.com>
2014-12-13 17:31:30 +0700

    Update Step 1.4

---
+++ Step1.4.py
@@ -0,0 +1,17 @@
+t = input('')
+if t == 'треугольник':
+    a = int(input(''))
+    b = int(input(''))
+    c = int(input(''))
+    p = float((a+b+c)/2)
+    s = float ((p*(p-a)*(p-b)*(p-c))**0.5)
+    print (s)
+elif t == 'прямоугольник':
+    a = int(input(''))
+    b = int(input(''))
+    s = float(a*b)
+    print (s)
+elif t == 'круг':
+    r = int(input(''))
+    s = float(3.14 * (r**2))
+    print (s)
